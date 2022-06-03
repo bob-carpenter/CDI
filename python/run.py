858d0b1081e2a30ce73c9a3f94516d58556ae2b8
Brian Ward <bward@flatironinstitute.org>
2022-06-03 11:53:03 -0400

    Add ability to initialize at true image

--- python/run.py
+++ python/run.py
@@ -3,13 +3,22 @@ import scipy.io as scio
 import cmdstanpy
 import numpy as np
 import pathlib
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
+def rgb2gray(rgb):
 
-REPO_DIR =   pathlib.Path(__file__).parent.parent.resolve()
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray
+
+REPO_DIR = pathlib.Path(__file__).parent.parent.resolve()
 
 STAN_FILE = REPO_DIR / "stan" / "holo-cdi.stan"
 REF_FILE = REPO_DIR / "data" / "URA_256.csv"
 DATA_FILE = REPO_DIR / "data" / "Yt_data.mat"
+TRUE_IMAGE =  REPO_DIR / "data" / "mimivirus.png"
 
 
 model = cmdstanpy.CmdStanModel(stan_file=STAN_FILE)
@@ -27,8 +36,14 @@ N_p = data['Np'][0,0]
 # r = floor(0.5*0.05*np.mean([M1,M2]))
 # N_p = 10 # ???
 
+x_init_true = rgb2gray(mpimg.imread(TRUE_IMAGE))
+
 data = {"N":N, "R":R, "M1":M1, "M2":M2, "Y_tilde":Y_tilde, "r":r, "N_p":N_p}
 
 if __name__ == "__main__":
-    fit = model.sample(data=data, chains=1, show_console=True)
+    fit = model.optimize(data=data, show_console=True,
+        inits=1#{'X': x_init_true}
+    )
     fit.save_csvfiles('.')
+    plt.imshow(fit.stan_variable("X"))
+    plt.show()
