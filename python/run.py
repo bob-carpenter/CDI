2bc88c4f3f38f8e8075835e8c1716e98072cb1bb
Brian Ward <bward@flatironinstitute.org>
2022-06-07 12:21:20 -0400

    More sizes, configs

--- python/run.py
+++ python/run.py
@@ -1,11 +1,14 @@
-from math import floor
-import scipy.io as scio
-import cmdstanpy
-import numpy as np
+import datetime
+import os
 import pathlib
+import time
+from math import floor
+
+import cmdstanpy
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-import time
+import numpy as np
+import scipy.io as scio
 
 
 def rgb2gray(rgb):
@@ -15,17 +18,10 @@ def rgb2gray(rgb):
     return gray
 
 
-def side_by_side(first, second):
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.imshow(first[0], cmap="gray")
-    ax1.set_title(first[1])
-    ax2.imshow(second[0], cmap="gray")
-    ax2.set_title(second[1])
-    plt.show()
+SIZE = "256"  # 32, 64, 128, 192, 256
+NOISE = "full"  # noiseless, low_photon, beamstop, full
+METHOD = "OPTIMIZE"  # OPTIMIZE, SAMPLE
 
-
-SIZE = "32"
-NOISE = "low_photon"
 
 REPO_DIR = pathlib.Path(__file__).parent.parent.resolve()
 
@@ -33,7 +29,8 @@ STAN_FILE = REPO_DIR / "stan" / "holo-cdi.stan"
 REF_FILE = REPO_DIR / "data" / SIZE / f"URA_{SIZE}.csv"
 DATA_FILE = REPO_DIR / "data" / SIZE / NOISE / "Yt_data.mat"
 TRUE_IMAGE = REPO_DIR / "data" / SIZE / "mimivirus.png"
-
+RESULT_DIR = REPO_DIR / "results" / METHOD / SIZE / NOISE
+RESULT_DIR.mkdir(parents=True, exist_ok=True)
 
 model = cmdstanpy.CmdStanModel(stan_file=STAN_FILE)
 
@@ -47,6 +44,18 @@ r = int(data["r"][0, 0])
 N_p = data["Np"][0, 0]
 
 
+def side_by_side(first, second, save=False):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(first[0], cmap="gray")
+    ax1.set_title(first[1])
+    ax2.imshow(second[0], cmap="gray")
+    ax2.set_title(second[1])
+    fig.suptitle(f"{SIZE}x{SIZE} - N_p: {N_p} - r: {r}")
+    if save:
+        fig.savefig(RESULT_DIR / f"recovery-{datetime.datetime.now():%Y%m%d%H%M%S}")
+    plt.show()
+
+
 x_init_true = rgb2gray(mpimg.imread(TRUE_IMAGE))
 
 data = {"N": N, "R": R, "M1": M1, "M2": M2, "Y_tilde": Y_tilde, "r": r, "N_p": N_p}
@@ -57,25 +66,33 @@ if __name__ == "__main__":
     # sanity check:
     side_by_side((x_init_true, "Ground truth"), (R, "reference"))
 
-    # fit = model.optimize(
-    #     data=data,
-    #     inits=1,
-    #     # inits={"X": x_init_true},
-    #     show_console=True,
-    #     save_iterations=True,
-    #     output_dir='.'
-    # )
-    # side_by_side((x_init_true, "True X"), (fit.stan_variable("X"), "Recovered X"))
-
-    before = time.perf_counter()
-    fit = model.sample(
-        data=data,
-        chains=2,
-        inits=1,
-        # inits={"X": x_init_true},
-        show_console=True,
-    )
-    after = time.perf_counter()
-    print(f'Sampling took {after - before:0.2f} seconds')
-    fit.save_csvfiles(".")
-    side_by_side((x_init_true, "True X"), (fit.stan_variable("X").mean(axis=0), "Recovered X"))
+    if METHOD == "OPTIMIZE":
+        fit = model.optimize(
+            data=data,
+            inits=1,
+            # inits={"X": x_init_true},
+            show_console=True,
+            save_iterations=True,
+            output_dir=RESULT_DIR,
+        )
+        side_by_side(
+            (x_init_true, "True X"), (fit.stan_variable("X"), "Recovered X"), True
+        )
+    elif METHOD == "SAMPLE":
+        before = time.perf_counter()
+        fit = model.sample(
+            data=data,
+            chains=2,
+            inits=1,
+            # inits={"X": x_init_true},
+            show_console=True,
+            output_dir=RESULT_DIR,
+            save_warmup=True,
+        )
+        after = time.perf_counter()
+        print(f"Sampling took {after - before:0.2f} seconds")
+        side_by_side(
+            (x_init_true, "True X"),
+            (fit.stan_variable("X").mean(axis=0), "Recovered X"),
+            True,
+        )
